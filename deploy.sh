512f868396ddbb7093efec7901db5ac4d0fc7af9
Mohamed khamkham <mohamkham1999@gmail.com>
2023-12-08 10:41:54 +0100

    repo

--- deploy.sh
+++ deploy.sh
@@ -17,10 +17,9 @@ git checkout -b main
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:Mohamedkhamkham/Mohamedkhamkham.github.io.git main
+# if you are deploying to https://<USERNAME>.github.io·git push -f git@github.com:Mohamedkhamkham/share-car.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:Mohamedkhamkham/share-car.git main:gh-pages
 
 cd -
