122d1c4b0be397f09548f43c514bf1efdab7a493
Mohamed khamkham <mohamkham1999@gmail.com>
2023-12-15 14:06:15 +0100

    fixed carousel

--- src/consts/trip-consts.js
+++ src/consts/trip-consts.js
@@ -1,5 +1,5 @@
 export const mapImages = [
-    { src: '../../assets/AhorroCo2.png'},
-    { src: '/src/assets/Ahorrocostes.png'},
-    { src: '/src/assets/reduccionTrafico.png'}
+    { src: ' /src/assets/AhorroCo2.png' },
+    { src: '/src/assets/Ahorrocostes.png' },
+    { src: '/src/assets/reduccionTrafico.png' }
 ]
